a36f13234a2cb5dc1e5b010674258d00627a3a64
Harsh Kaushik <harshkaushik096@gmail.com>
2026-01-15 00:12:14 +0530

    made changes in server.js

--- server/server.js
+++ server/server.js
@@ -15,4 +15,8 @@ cloudinary.config({
 
 // export const handler = serverless(app);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+});
